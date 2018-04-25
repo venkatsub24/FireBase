e37f4c2377d92cf767fd88ac8c6b0a77521635e0
Venkatesh Subramanian <venkatsmani93@gmail.com>
2018-04-25 21:15:23 +0530

    Getting data from google FIreBase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+
+interface Users {
+  name: string;
+  Place: string;
+  Profession : string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
+
+  usersCol: AngularFirestoreCollection<Users>;
+  users: Observable<Users[]>;
+
+  constructor(private afs: AngularFirestore) {}
+
+  ngOnInit() {
+    this.usersCol = this.afs.collection('users');
+    this.users = this.usersCol.valueChanges();
+  }
+
   title = 'app';
 }
