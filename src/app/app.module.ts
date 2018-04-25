064e2a86d5f081ab840cc4fb735a3f8668308c39
Venkatesh Subramanian <venkatsmani93@gmail.com>
2018-04-25 20:57:19 +0530

    importing firebase dependencies to project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
 
 import { AppComponent } from './app.component';
 
+var firebaseConfig = {
+  apiKey: "AIzaSyD__RVp_z5Mic0KaVfYbNgiokfhsYkFgk8",
+  authDomain: "my-realtime-app.firebaseapp.com",
+  databaseURL: "https://my-realtime-app.firebaseio.com",
+  projectId: "my-realtime-app",
+  storageBucket: "",
+  messagingSenderId: "937938850926"
+};
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
